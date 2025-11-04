7777af7d0112289e79be9ba003eb4ca62a2bf499
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-11-04 19:48:44 +0300

    decomposition playlistItem

---
+++ src/features/playlists/ui/PlaylistCover.tsx
@@ -0,0 +1,46 @@
+import {useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation} from "@/features/playlists/api/playlistsApi.ts";
+import type {Images} from "@/common/types";
+import type {ChangeEvent} from "react";
+import s from './PlaylistItem.module.css'
+import defaultCover from '@/assets/img/default-playlist-cover.png'
+
+type Props = {
+    playlistId: string
+    images: Images
+}
+
+export const PlaylistCover = ({ images, playlistId }: Props) => {
+    const originalCover = images.main?.find(img => img.type === 'original')
+    const src = originalCover ? originalCover?.url : defaultCover
+
+    const [uploadCover] = useUploadPlaylistCoverMutation()
+    const [deleteCover] = useDeletePlaylistCoverMutation()
+
+    const uploadCoverHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const maxSize = 1024 * 1024 // 1 MB
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
+
+        const file = event.target.files?.length && event.target.files[0]
+        if (!file) return
+
+        if (!allowedTypes.includes(file.type)) {
+            alert('Only JPEG, PNG or GIF images are allowed')
+        }
+
+        if (file.size > maxSize) {
+            alert(`The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`)
+        }
+
+        uploadCover({ playlistId, file })
+    }
+
+    const deleteCoverHandler = () => deleteCover({ playlistId })
+
+    return (
+        <div>
+            <img src={src} alt={'cover'} width={'100px'} className={s.cover} />
+            <input type="file" accept="image/jpeg,image/png,image/gif" onChange={uploadCoverHandler} />
+            {originalCover && <button onClick={() => deleteCoverHandler()}>delete cover</button>}
+        </div>
+    )
+}
--- src/features/playlists/ui/PlaylistItem.tsx
+++ src/features/playlists/ui/PlaylistItem.tsx
@@ -1,8 +1,6 @@
 import type {PlaylistData} from "@/features/playlists/api/playlistsApi.types.ts";
-import defaultCover from '@/assets/img/default-playlist-cover.png'
-import s from './PlaylistItem.module.css'
-import {useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation} from "@/features/playlists/api/playlistsApi.ts";
-import type {ChangeEvent} from 'react'
+import {PlaylistCover} from "@/features/playlists/ui/PlaylistCover.tsx";
+import {PlaylistDescription} from "@/features/playlists/ui/PlaylistDescription.tsx";
 
 type Props = {
     playlist: PlaylistData
@@ -12,41 +10,10 @@ type Props = {
 
 export const PlaylistItem = ({ playlist, editPlaylist, deletePlaylist }: Props) => {
 
-    const [updatePlaylistCover] = useUploadPlaylistCoverMutation()
-    const [deletePlaylistCover] = useDeletePlaylistCoverMutation()
-
-    const originalCover = playlist.attributes.images.main.find(img => img.type === 'original')
-    const src = originalCover ? originalCover.url : defaultCover
-
-    const uploadCoverHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        const maxSize = 1024 * 1024 // 1 MB
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
-
-        const file = event.target.files?.length && event.target.files[0]
-        if (!file) return
-
-        if (!allowedTypes.includes(file.type)) {
-            alert('Only JPEG, PNG or GIF images are allowed')
-            return
-        }
-        if (file.size > maxSize) {
-            alert(`The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`)
-            return
-        }
-        updatePlaylistCover({ playlistId: playlist.id, file })
-    }
-
-    const deletePlaylistHandler = () =>deletePlaylistCover({ playlistId: playlist.id })
-
-
     return (
         <div>
-            <img src={src} alt="cover" width={'240px'} className={s.cover} />
-            {originalCover && <button onClick={deletePlaylistHandler}>delete img</button>}
-            <input type={'file'} accept={'image/png, image/jpeg, image/gif'} onChange={uploadCoverHandler}/>
-            <div>title: {playlist.attributes.title}</div>
-            <div>description: {playlist.attributes.description}</div>
-            <div>userName: {playlist.attributes.user.name}</div>
+            <PlaylistCover playlistId={playlist.id} images={playlist.attributes.images}/>
+            <PlaylistDescription attributes={playlist.attributes}/>
             <button onClick={() => deletePlaylist(playlist.id)}>delete</button>
             <button onClick={() => editPlaylist(playlist)}>update</button>
         </div>
