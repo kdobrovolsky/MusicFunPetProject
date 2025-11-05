6b59f9e2ff0be5c3436e79f258a4ec854317fbaa
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-11-05 21:13:42 +0300

    add infinityScroll

---
+++ src/features/tracks/api/tracksApi.ts
@@ -0,0 +1,21 @@
+import {baseApi} from "@/app/api/baseApi.ts";
+import type {FetchTracksResponse} from "@/features/tracks/api/tracksApi.types.ts";
+export const tracksApi = baseApi.injectEndpoints({
+    endpoints: build => ({
+        fetchTracks: build.infiniteQuery<FetchTracksResponse, void, string | undefined>({
+            infiniteQueryOptions: {
+                initialPageParam: undefined,
+                getNextPageParam: lastPage => {
+                    return lastPage.meta.nextCursor || undefined
+                },
+            },
+            query: ({ pageParam }) => {
+                return {
+                    url: 'playlists/tracks',
+                    params: { cursor: pageParam, pageSize: 5, paginationType: 'cursor' },
+                }
+            },
+        }),
+    }),
+})
+export const { useFetchTracksInfiniteQuery } = tracksApi
--- src/features/tracks/ui/TraksPage.tsx
+++ src/features/tracks/ui/TraksPage.tsx
@@ -1,7 +1,22 @@
+import {useFetchTracksInfiniteQuery} from "@/features/tracks/api/tracksApi.ts";
+import {useInfiniteScroll} from "@/common/hooks";
+import {TracksList} from "@/features/tracks/ui/TraksList/TracksList.tsx";
+import {LoadingTrigger} from "@/features/tracks/ui/LoadingTrigger/LoadingTrigger.tsx";
+
 export const TracksPage = () => {
+
+    const {data,hasNextPage,isFetching,fetchNextPage,isFetchingNextPage} = useFetchTracksInfiniteQuery()
+    const pages = data?.pages.flatMap(page => page.data) || []
+
+    const {observerRef} = useInfiniteScroll({fetchNextPage,hasNextPage,isFetching})
+
     return (
         <div>
             <h1>Tracks page</h1>
+                <TracksList tracks={pages}/>
+                <LoadingTrigger observerRef={observerRef} isFetchingNextPage={isFetchingNextPage}/>
+                {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
+
         </div>
     )
 }
