77ae8ca8c26edf17bb7a7c9b55258857e67508a2
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-11-08 15:31:07 +0300

    test commit

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -11,6 +11,7 @@ export const baseApi = createApi({
         headers: {
             'API-KEY': import.meta.env.VITE_API_KEY,
         },
+
         prepareHeaders: headers => {
             headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
             return headers
